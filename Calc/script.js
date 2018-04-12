36fe8be54cfe5ec66a62a266c2ba8e65de440c3d
Максим Крыжановский <just_my_mail@coolsite.net>
2018-04-12 21:43:30 +0300

    demo

--- Calc/script.js
+++ Calc/script.js
@@ -3,6 +3,7 @@ function Form(el){
     let resultEl = elem.getElementsByClassName('result')[0]; //нашли блок, в который будем выводить результат
     let inputs = [];
     var button=elem.getElementsByTagName('input')[1];
+    let enter =false;
 	
     //вспомогательные (приватные) функции	
 	function pushElemsByTag(tag){ //добавляем элементы формы по тегу к inputs
@@ -17,10 +18,9 @@ function Form(el){
 			inputs.push(inp); //добавляем элемент в массив
         }
 	}
-    function total(){ //складываем value всех элементов 
-        let text = document.getElementsByTagName("input")[0];
-		let val = text.value;
-		let res ="";
+	function binary(val)
+	{
+    	let res ="";
 		while(val != 0)
 		{
 			if(val%2==0){
@@ -30,7 +30,44 @@ function Form(el){
 			}
 			val=parseInt(val/2);
 		}
-        return res.split("").reverse().join("");;
+        return res.split("").reverse().join("");
+    };
+    function decimal(val)
+    {
+    	let res =0;
+    	for(var i=val.length-1,c = 0; i>=0; i--, c++)
+    	{
+    		if(val[i] <0 || val[i] >1) return "Странный у вас двоичный код";
+    		if(val[i] == 1)
+    		{
+    			res+=Math.pow(2,c);
+    			console.log(res);
+    		}
+    	}
+    	return res;
+    }
+    function total(){ //складываем value всех элементов
+    	let sys = elem.getElementsByTagName('label')[1];
+    	let sel = document.getElementsByTagName("select")[0];
+    	sel = sel.value; 
+    	let val = document.getElementsByTagName("input")[0];
+		val = val.value; 
+    	switch(sel){
+    		case "0": return "Вы не выбрали систему"; 
+    		case "1":sys.innerHTML = "Перевести в бинарный код";return binary(val); break;
+    		case "2":sys.innerHTML = "Перевести в десятичный код"; return decimal(val); break;
+    	}
+		/*let res ="";
+		while(val != 0)
+		{
+			if(val%2==0){
+				res+='0';
+			}else{
+				res+='1';
+			}
+			val=parseInt(val/2);
+		}
+        return res.split("").reverse().join("");;*/
     };
 	button.onclick=showResult();
 	function showResult(){
@@ -56,7 +93,14 @@ function Input(el){
 	}
 	this.value = getValue;
 }
-
-
+function Radio(el){
+    Input.call(this);
+    function getValue(){
+		if (el.checked)//если элемент выбран, то берем его значение, если нет - возвращаем 0
+			return el.value;
+		return 0;
+    }
+    this.value = getValue;
+}
 let form = new Form(document.forms["notation"]);
 form.init();
