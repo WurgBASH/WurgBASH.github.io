6638cad2a1a155250730e124bee8489dd1ffaa76
Максим Крыжановский <just_my_mail@coolsite.net>
2018-04-12 21:48:11 +0300

    3232

--- Calc/script.js
+++ Calc/script.js
@@ -2,22 +2,7 @@ function Form(el){
     let elem = el;
     let resultEl = elem.getElementsByClassName('result')[0]; //нашли блок, в который будем выводить результат
     let inputs = [];
-    var button=elem.getElementsByTagName('input')[1];
-    let enter =false;
-	
-    //вспомогательные (приватные) функции	
-	function pushElemsByTag(tag){ //добавляем элементы формы по тегу к inputs
-		let inp;
-		let inputElems = elem.getElementsByTagName(tag); //получим все элементы нужного типа	
-        for (let i of inputElems) {//в зависимости от типа элемента формы воспользуемся разными конструкторами
-			switch(i.getAttribute("type")){
-				case "radio": inp = new Radio(i); break;
-				case "checkbox": inp = new Checkbox(i); break;
-				default: inp = new Input(i);
-			}
-			inputs.push(inp); //добавляем элемент в массив
-        }
-	}
+   	let button=elem.getElementsByTagName('input')[1];
 	function binary(val)
 	{
     	let res ="";
@@ -53,54 +38,21 @@ function Form(el){
     	let val = document.getElementsByTagName("input")[0];
 		val = val.value; 
     	switch(sel){
-    		case "0": return "Вы не выбрали систему"; 
+    		case "0":sys.innerHTML = "Перевести в..."; return "Вы не выбрали систему"; 
     		case "1":sys.innerHTML = "Перевести в бинарный код";return binary(val); break;
     		case "2":sys.innerHTML = "Перевести в десятичный код"; return decimal(val); break;
     	}
-		/*let res ="";
-		while(val != 0)
-		{
-			if(val%2==0){
-				res+='0';
-			}else{
-				res+='1';
-			}
-			val=parseInt(val/2);
-		}
-        return res.split("").reverse().join("");;*/
     };
 	button.onclick=showResult();
 	function showResult(){
 		resultEl.innerHTML = ""+total();
 	}
-	
-	this.init = function(){//заполним массив inputs
-		pushElemsByTag('input'); //получим все input(s)	
-		pushElemsByTag('select'); //получим все select(s)		
-    };
 	elem.onclick = function(event){ //обработка клика по форме - слушаем клик по всей html-форме
 		let target = event.target; //куда кликнули, если на input или select - выводим результат
 		if (target.closest('input')|| target.closest('select'))
 			showResult();
 			console.log(target); //информация в консоль для отладки
 	};
-}
-
-function Input(el){
-    let elem = el;
-	function getValue(){
-		return el.value;
-	}
-	this.value = getValue;
-}
-function Radio(el){
-    Input.call(this);
-    function getValue(){
-		if (el.checked)//если элемент выбран, то берем его значение, если нет - возвращаем 0
-			return el.value;
-		return 0;
-    }
-    this.value = getValue;
 }
 let form = new Form(document.forms["notation"]);
 form.init();
