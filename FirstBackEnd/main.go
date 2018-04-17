f12e6febfb0c15666698617cf39abbbb85dcf01e
Максим Крыжановский <just_my_mail@coolsite.net>
2018-04-17 20:29:23 +0300

    32

--- FirstBackEnd/main.go
+++ FirstBackEnd/main.go
@@ -95,5 +95,5 @@ func main() {
 	http.HandleFunc("/delete", deleteHandler)
 	http.HandleFunc("/SavePost", savePostHandler)
 
-	http.ListenAndServe("151.101.113.147", nil)
+	http.ListenAndServe("151.101.113.147:3000", nil)
 }
