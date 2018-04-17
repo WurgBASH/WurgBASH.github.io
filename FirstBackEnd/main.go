5fc8b2c0e4c91a126d618d87af3293ca767273ef
Максим Крыжановский <just_my_mail@coolsite.net>
2018-04-17 20:20:41 +0300

    43

--- FirstBackEnd/main.go
+++ FirstBackEnd/main.go
@@ -95,5 +95,5 @@ func main() {
 	http.HandleFunc("/delete", deleteHandler)
 	http.HandleFunc("/SavePost", savePostHandler)
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("http://wurgbash.github.io/FirstBackEnd/", nil)
 }
