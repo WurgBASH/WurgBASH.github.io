7d671ef5532aac1e790fb2d76d34052ab80c4df2
maxim-kr-solidlogic <just_my_mail@coolsite.net>
2023-04-14 12:29:44 +0200

    delete all old files

--- OldFashionCode/chat-master/keygen/README.md
+++
@@ -1,3 +0,0 @@
-# keygen: API key generator
-
-A command-line utility to generate an API key for [Tinode server](../server/)
--- OldFashionCode/chat-master/pbx/generate-python.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m grpc_tools.protoc -I../pbx --python_out=../pbx --grpc_python_out=../pbx ../pbx/model.proto
